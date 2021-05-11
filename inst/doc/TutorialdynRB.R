254cff6ec32dc4a952bc1a64bd0b16ca6d8e3ec5
Florian Griessenberger <florian.griessenberger@sbg.ac.at>
2021-05-11 08:20:13 +0000

    version 0.16

--- inst/doc/TutorialdynRB.R
+++ inst/doc/TutorialdynRB.R
@@ -1,25 +1,37 @@
+## ----library, message=FALSE---------------------------------------------------
 library(dynRB)
 library(ggplot2)
 library(reshape2)
 library(vegan)
 library(RColorBrewer)
 
+## ----data---------------------------------------------------------------------
 data(finch)
 head(finch)[,1:5]
 
+## ----finch, results = "hide"--------------------------------------------------
 r <- dynRB_VPa(finch)
 
+## ----result-------------------------------------------------------------------
 res <- r$result
 res[1:6, 1:5]
 
+## ----result2------------------------------------------------------------------
 res[1:6, c(1:2, 6:11)]
 
+## ----result3, results = "hide"------------------------------------------------
 r <- dynRB_Pn(finch)
+
+## ----result31-----------------------------------------------------------------
 head(r$result)
 
+## ----result4, results = "hide"------------------------------------------------
 r <- dynRB_Vn(finch)
+
+## ----result41-----------------------------------------------------------------
 head(r$result)
 
+## ----result5, results = "hide"------------------------------------------------
 r <- dynRB_VPa(finch)
 # aggregation method product  
 om1 <- reshape(r$result[,1:3], direction="wide", idvar="V1", timevar="V2") 
@@ -28,14 +40,18 @@ om2 <- reshape(r$result[,c(1,2,4)], direction="wide", idvar="V1", timevar="V2")
 #aggregation method gmean  
 om3 <- reshape(r$result[,c(1,2,5)], direction="wide", idvar="V1", timevar="V2") 
 
+
+## ----result51-----------------------------------------------------------------
 om1
 
+## ----plot1, results = "hide", fig.width=7, fig.height=5, message=FALSE, warning=FALSE----
 r <- dynRB_VPa(finch)
 #aggregation method mean  
 om <- reshape(r$result[,c(1,2,4)], direction="wide", idvar="V1", timevar="V2") 
 mantel(as.dist(om[2:ncol(om)]), as.dist(t(om[2:ncol(om)])), permutations = 1000)
 plot(as.dist(om[2:ncol(om)]), as.dist(t(om[2:ncol(om)]))) 
 
+## ----plot2, results = "hide", fig.width=6, fig.height=4-----------------------
 r <- dynRB_VPa(finch)  #main function of dynRB to calculate size and overlap of hypervolumes
 result <- r$result
 Overlap <- as.numeric(ifelse(result$V1 == result$V2, "NA", result$port_prod))  
@@ -46,6 +62,7 @@ ggplot(Result2, aes(x = V1, y = V2)) +
   geom_tile(data = subset(Result2, !is.na(Overlap)), aes(fill = Overlap), color="black") +
   geom_tile(data = subset(Result2,  is.na(Overlap)), fill = "lightgrey", color="black")
 
+## ----plot3, results = "hide", fig.width=6, fig.height=4-----------------------
 r <- dynRB_VPa(finch)  
 theme_change <- theme(
   plot.background = element_blank(),
@@ -74,5 +91,4 @@ ggplot(Result2, aes(x = V1, y = V2)) +
   scale_fill_gradientn(colours=col1(8), breaks=breaks, guide="colorbar",  
                        limits=c(min(Overlap, na.rm=TRUE),max(Overlap, na.rm=TRUE))) +
   theme_change
-
 
